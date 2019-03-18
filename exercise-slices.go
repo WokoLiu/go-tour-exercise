0f507b4facc827833e4fa7e1de960f54cfbd87b3
刘玉龙 <ylliu@hillinsight.com>
2019-03-18 16:30:28 +0800

    exercese 2

---
+++ exercise-slices.go
@@ -0,0 +1,21 @@
+package main
+
+import "golang.org/x/tour/pic"
+
+func Pic(dx, dy int) [][]uint8 {
+	res := [][]uint8{}
+	for x := 0; x < dy; x++ {
+		temp := []uint8{}
+		for y := 0; y < dx; y++ {
+			temp = append(temp, uint8(x^y))
+			//temp = append(temp, uint8((x+y)/2))
+			//temp = append(temp, uint8(x*y))
+		}
+		res = append(res, temp)
+	}
+	return res
+}
+
+func main() {
+	pic.Show(Pic)
+}
